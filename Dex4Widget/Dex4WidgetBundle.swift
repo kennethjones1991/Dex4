87327e45b3a2027297b922e077cec1ec0165c6ae
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-31 11:50:30 -0700

    End of lesson 17; add WidgetKit extension

---
+++ Dex4Widget/Dex4WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Dex4WidgetBundle.swift
+//  Dex4Widget
+//
+//  Created by Kenneth Jones on 1/31/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Dex4WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Dex4Widget()
+    }
+}
